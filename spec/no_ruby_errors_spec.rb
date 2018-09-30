65567e9b60d30a73729d41e9275dcd9c7dfbec9e
Ranika Williams <rnkprice07@gmail.com>
2018-09-30 19:30:59 +0000

    Done.

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -11,7 +11,7 @@ describe "Not having any errors and being all green" do
   context 'SyntaxError' do
     it 'raises a SyntaxError for nonsensical code' do
       expect{
-        load './lib/a_syntax_error.rb'
+        load "./lib/a_syntax_error.rb"
       }.to_not raise_error
     end
   end
