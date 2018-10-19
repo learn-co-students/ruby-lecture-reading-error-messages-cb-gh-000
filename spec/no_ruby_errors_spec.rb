1ea35d7d30c800c3d772c18f71f042b459eb58cc
Khalid Mohamed <mohamedkhalid149@hotmail.com>
2018-10-19 01:31:01 +0000

    Done.

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -16,7 +16,7 @@ describe "Not having any errors and being all green" do
     end
   end
 
-  context 'TypeError' do
+  context 'TypeError' do 
     it 'raises a TypeError for objects of the wrong type' do
       expect{
         load './lib/a_type_error.rb'
